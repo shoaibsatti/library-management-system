5fd8f353884a6420863800c22b81792bbac0a5f5
Izhan10 <164905315+Izhan10@users.noreply.github.com>
2026-06-15 09:59:24 +0500

    Add Book class with initialization and string representation

---
+++ book.py
@@ -0,0 +1,10 @@
+class Book:
+    def __init__(self, book_id, title, author):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.is_borrowed = False
+
+    def __str__(self):
+        status = "Borrowed" if self.is_borrowed else "Available"
+        return f"[{self.book_id}] {self.title} by {self.author} - {status}"
